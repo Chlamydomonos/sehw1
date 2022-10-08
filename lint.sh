f58e8ce77582d9fb03d51811db0fe64251508c2a
Chlamydomonos <2457242458@qq.com>
2022-10-08 17:35:06 +0800

    完成lint.sh

--- lint.sh
+++ lint.sh
@@ -1,10 +1,49 @@
+#! /bin/bash
+
 # TODO: add autopep8 here.
 
+autopep8_all() {
+    local root="$1"
+    local fileName=""
+    for file in `ls $1`
+    do
+        fileName=${root}"/"${file}
+        if [ -d $fileName ]
+        then
+            autopep8_all $fileName
+        elif [ "${fileName##*.}" == "py" ]
+        then
+            autopep8 --in-place $fileName
+        fi
+    done
+}
+
+autopep8_all .
 
 # TODO: add autoflake here.
 
+autoflake_all() {
+    local root="$1"
+    local fileName=""
+    for file in `ls $1`
+    do
+        fileName=${root}"/"${file}
+        if [ -d $fileName ]
+        then
+            autoflake_all $fileName
+        elif [ "${fileName##*.}" == "py" ]
+        then
+            autoflake --in-place $fileName
+        fi
+    done
+}
+
+autoflake_all .
 
 # TODO: add isort here.
 
+isort .
 
 # TODO: add flake8 here.
+
+flake8 .
